8e8972f6be3856e6cd7096b7062e73c3901fd6a2
sammsiontir <chengfu@umich.edu>
2014-05-05 15:17:52 -0400

    java_test

---
+++ java_test/Ex2.java
@@ -0,0 +1,14 @@
+public class Ex2{
+   public static void main(string[] args) {
+      int i1 = 3;
+      int i2 = 4;
+      if (i1>i2)
+         System.out.println("i1 is larger");
+      else if (i1<i2)
+         System.out.println("i2 is larger");
+      else {
+         System.out.println("Both are equal!");
+      }
+      return;
+   }
+}
